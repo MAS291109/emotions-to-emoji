dc89dd16500158f904a4435d088bb6faea8b0c7e
MAS291109 <81005913+MAS291109@users.noreply.github.com>
2021-12-11 12:51:29 +0530

    Add files via upload
    webcam and model loaded.

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set({
+    width:350,
+    height:320,
+    image_format: 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_img" src="'+data_uri+'"/>';
+    });
+
+}
+    console.log('ml5 version', ml5.version);
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZrFWMOeLV/model.json', modelLoaded);
+
+    function modelLoaded(){
+        console.log('Model Loaded');
+    }
+
+    function speak(){
+        var synth = window.speechSynthesis;
+        speak_data1 = "The first prediction is " + prediction_1;
+        speak_data2 = "The second prediction is " + prediction_2;
+        var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
+        synth.speak(utterThis);
+    }
+
